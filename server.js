1e0739ef6373001b9c36f08d459f6617f6814996
klaytonme <claytonwiley@seattleacademy.org>
2019-03-11 22:46:53 -0700

    Update several files (still not working)

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
@@ -10,9 +11,12 @@ app.use(express.static('dist'));
 
 app.use('/dist', express.static('dist'));
 
+io.on('connection', function(socket){
+  console.log('a user connected');
+});
 
 //identify unused port between 1520 and 1540
-var port = 1541;
+var port = 1538;
 http.listen(port, function(){
   console.log('listening on https://seattleacademy.software/'+port);
 });
